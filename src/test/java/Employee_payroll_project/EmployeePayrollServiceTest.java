a91736f85295992435a46c07339d37e45f166f00
VCHINIMI <vinaychinimilli@gmail.com>
2020-11-01 12:08:47 +0530

    [Vinay]Added: Test case for reading from database

--- src/test/java/Employee_payroll_project/EmployeePayrollServiceTest.java
+++ src/test/java/Employee_payroll_project/EmployeePayrollServiceTest.java
@@ -1,16 +1,19 @@
 package Employee_payroll_project;
 
 import static org.junit.Assert.*;
-
 import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import org.junit.Test;
-
 import Employee_payroll_project.EmployeePayrollService.IOService;
 
-
 public class EmployeePayrollServiceTest {
-	
+
+	@Test
+	public void givenEmployeePayrollInDB_ShouldMatchEmployeeCount() throws EmployeeException {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+		assertEquals(3, employeePayrollData.size());
+	}
 }
